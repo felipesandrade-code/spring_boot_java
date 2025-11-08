f7ab972370bf0fe66e3c3417c1da72658f16d0a4
Felipe Andrade <feandrade.work@gmail.com>
2025-11-08 14:21:25 -0300

    ok.

--- src/main/java/br/com/fiap/calorias/controller/AuthController.java
+++ src/main/java/br/com/fiap/calorias/controller/AuthController.java
@@ -3,22 +3,14 @@ package br.com.fiap.calorias.controller;
 import br.com.fiap.calorias.dto.LoginDto;
 import br.com.fiap.calorias.dto.UsuarioCadastroDto;
 import br.com.fiap.calorias.dto.UsuarioExibirDto;
-import br.com.fiap.calorias.model.Usuario;
 import br.com.fiap.calorias.service.UsuarioService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.*;
-
-import javax.swing.text.html.Option;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
@@ -35,7 +27,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-        public ResponseEntity login(@RequestBody @Valid LoginDto loginDto){
+        public ResponseEntity login(@RequestBody @Valid  LoginDto loginDto){
             UsernamePasswordAuthenticationToken usernamePassword = new UsernamePasswordAuthenticationToken(
                     loginDto.email(),
                     loginDto.senha()
--- src/main/java/br/com/fiap/calorias/repository/UsuarioRepository.java
+++ src/main/java/br/com/fiap/calorias/repository/UsuarioRepository.java
@@ -1,6 +1,5 @@
 package br.com.fiap.calorias.repository;
 
-import br.com.fiap.calorias.dto.UsuarioExibirDto;
 import br.com.fiap.calorias.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,7 +11,6 @@ import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    @Query("SELECT u.email from Usuario u WHERE u.email = :email")
     UserDetails findByEmail(@Param("email") String email);
 
     @Query("select u.nome from Usuario u where u.nome = :nome")
